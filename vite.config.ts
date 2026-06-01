403084f37e51e7404a22c4384036389dc4845026
Bobby Earl <bobby@simplyearl.com>
2026-06-01 09:46:58 -0400

    chore: code hygiene

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
   plugins: [tailwindcss(), ViteMinifyPlugin()],
